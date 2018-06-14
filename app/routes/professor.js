982d42703e0400e5310ef86a4ca3242a636963b3
danieloliveiras <31636778+danieloliveiras@users.noreply.github.com>
2018-06-14 00:01:19 -0300

    Banco pronto

--- app/routes/professor.js
+++ app/routes/professor.js
@@ -2,7 +2,7 @@ let controller = require('../controllers/professor');
 let auth = require('../controllers/auth');
 
 module.exports = function(app){
-    app.post('/api/professores/entrar', auth.logar);
+    app.post('/api/professores/login', auth.logar);
     app.post('/api/professores', controller.inserirProfessor);
     app.use('/api/professores', auth.checar);
     app.get('/api/professores', controller.listarProfessores);
--- config/express.js
+++ config/express.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+const path = require('path');
 
 let professorRouter = require('../app/routes/professor');
 let sessaoRouter = require('../app/routes/sessao');
@@ -9,7 +10,11 @@ module.exports = function(){
     app.set('port', 3000);
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false}));
+    app.use(express.static('./public'));
     professorRouter(app);
     sessaoRouter(app);
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(_dirname, '../public/index.html'));
+    })
     return app;
 }
