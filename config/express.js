705c77c01c38764a19fcd7f2353a095ccf78612c
DESKTOP-0P85KC4\Jayne <jaynecar.franca@hotmail.com>
2018-06-21 13:20:45 -0300

    tentando subir o projeto mais uma vez, sem o favicon

--- config/express.js
+++ config/express.js
@@ -1,13 +1,15 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 const path = require('path');
+const port = process.env.PORT || 5000
+
 
 let professorRouter = require('../app/routes/professor');
 let sessaoRouter = require('../app/routes/sessao');
 
 module.exports = function(){
     let app = express();
-    app.set('port', 3000);
+    app.set('port', port);
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false}));
     app.use(express.static('./public'));
--- main.js
+++ main.js
@@ -7,4 +7,4 @@ http.createServer(app).listen(app.get('port'), function(){
     console.log('Express Server escutando na porta ' + app.get('port'));
 });
 
-db('mongodb://localhost/arcos');
+db('mongodb://jaynecrvlh:jayne1997@ds163410.mlab.com:63410/arcos');
